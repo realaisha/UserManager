69a2f2112a0f672adaa7c5aea00637f4f023156e
realaisha <34861020+realaisha@users.noreply.github.com>
2017-12-26 14:57:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,5 +4,12 @@ var db = require('./db');
 
 var UserController = require('./user/UserController');
 app.use('/users', UserController);
+var express = require('express');
+var app = express();
+var db = require('./db');
 
+var UserController = require('./user/UserController');
+app.use('/users', UserController);
+
+module.exports = app;
 module.exports = app;
